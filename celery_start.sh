875249c64659a8eeb83fb7158da9e30fc52e8af2
firshme <firshme@MacBook-Pro-4.local>
2025-01-14 19:06:07 +0800

    init

--- celery_start.sh
+++ celery_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-nohup /opt/conda/envs/favicondb/bin/celery -A minitask.get_favicom.celery_app worker --loglevel=info &
+nohup /opt/conda/envs/favicondb/bin/celery -A minitask.get_favicom.celery_app worker --loglevel=info > sys.log 2>&1 &
 
-/opt/conda/envs/favicondb/bin/python main.py
+nohup /opt/conda/envs/favicondb/bin/python main.py  > sys.log 2>&1 &
+
+tail -f sys.log
 
